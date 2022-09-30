80176b07dfb404849eca7134c4fe760e7460fc6b
hbeooooooom <hbrepark@naver.com>
2022-10-01 03:56:20 +0900

    string int형변환 시도

--- Manager.cpp
+++ Manager.cpp
@@ -18,6 +18,7 @@ void Manager::Run(const char *filepath)
     char cmd[32];
     while (!fin.eof())
     {
+        Loaded_List *list= new Loaded_List;
         fin.getline(cmd, 100);
         char *command = strtok(cmd, " ");
         if (command == NULL)
@@ -25,12 +26,15 @@ void Manager::Run(const char *filepath)
         if (strcmp(command, "LOAD") == 0)
         {
             cout << "=======LOAD=======" << endl;
-            Loaded_List *list= new Loaded_List;
             Load(filepath, list);
-            
             cout << "===================" << endl;
         }
-        
+        else if(strcmp(command, "ADD")==0)
+        {
+            cout<<"=======ADD======="<<endl;
+            ADD(filepath, list);
+            cout<<"=================="<<endl;
+        }
     }
 }
 
@@ -41,8 +45,10 @@ void Manager::PrintError(Result result){
 
 Result Manager::Load(const char *filepath, Loaded_List *Load_list)
 {
+    int i=0;
     string filename = "img_files";
     string num, name,temp;
+    int changenum;
     fread.open("./img_files/filesnumbers.csv", ios::in);
     if(fread.fail()){
         cout<<"=======ERROR======="<<endl;
@@ -55,6 +61,8 @@ Result Manager::Load(const char *filepath, Loaded_List *Load_list)
         getline(fread, name, '.');
         getline(fread, temp, '\n');
         cout << name << "/" << num << endl;
+        //stringstream ssInt(num);
+        i=boost::lexical_cast<int>(num);
         Load_list->make_list(name,filename,num);
         
     }
--- Manager.h
+++ Manager.h
@@ -3,6 +3,7 @@
 #include<fstream>
 #include<iostream>
 #include<string>
+#include<cstdio>
 #include<cstring>
 #include"Result.h"
 #include"Queue.h"
@@ -10,15 +11,17 @@ using namespace std;
 
 class Manager
 {
-private:
-    Result Load(const char* filepath,Loaded_List* Load_list);
 public:
-    int nodecount = 0;
+ 
     ~Manager();
     std::ifstream fread;
     std::ifstream fin;
+    int nodecount = 0;
 
     void PrintError(Result result);
     void Run(const char* filepath);
-
+    
+private:
+    Result Load(const char* filepath,Loaded_List* Load_list);
+    Result ADD(const char* filepath,Loaded_List* Load_list);
 };
